9274b6167bb82d9930eb75fb0dad30b5e5747fac
diego <ddsgonas@gmail.com>
2025-12-11 21:03:16 -0500

    quitando estilo a header w-screen

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="shadow w-screen">
+  <header class="shadow">
     <nav>
       <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
